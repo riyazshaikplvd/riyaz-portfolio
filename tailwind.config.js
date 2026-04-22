84acb132f67ddd7fb8db04952da954936843651a
RIYAZ SHAIK <riyazshaikplvd@gmail.com>
2026-04-22 19:22:10 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // (safe even if you don't use pages)
   ],
   theme: {
     extend: {},
@@ -11,7 +12,6 @@ module.exports = {
     "text-cyan-400","text-blue-400","text-red-400","text-green-400",
     "text-orange-400","text-yellow-400","text-purple-400","text-pink-400",
     "text-teal-400","text-rose-400",
-
     "bg-cyan-500/10","bg-blue-500/10","bg-red-500/10","bg-green-500/10",
     "bg-orange-500/10","bg-yellow-500/10","bg-purple-500/10","bg-pink-500/10",
     "bg-teal-500/10","bg-rose-500/10"
