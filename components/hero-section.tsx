9d34bbbb7974432b530ad09d8429cbff40e96685
RIYAZ SHAIK <riyazshaikplvd@gmail.com>
2026-04-22 17:13:16 +0530

    Update hero-section.tsx

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -9,9 +9,8 @@ import Image from "next/image"
   { text: "SOC Analyst", color: "text-[#00FFFF] drop-shadow-[0_0_14px_#00FFFF]" },
   { text: "Incident Responder", color: "text-[#FF8C00] drop-shadow-[0_0_14px_#FF8C00]" },
   { text: "Threat Detection Expert", color: "text-[#FF0033] drop-shadow-[0_0_14px_#FF0033]" },
-  { text: "Cybersecurity Analyst", color: "text-[#00BFFF] drop-shadow-[0_0_14px_#00BFFF]" },
-  { text: "SIEM Monitoring Specialist", color: "text-[#C026FF] drop-shadow-[0_0_14px_#C026FF]" },
-  { text: "Security Operations Pro", color: "text-[#00FF7F] drop-shadow-[0_0_14px_#00FF7F]" },
+  { text: "Cybersecurity Analyst", color: "text-[#C026FF] drop-shadow-[0_0_14px_#C026FF]" },
+  { text: "Digital Forensics Expert", color: "text-[#00FF7F] drop-shadow-[0_0_14px_#00FF7F]" },
 ]
 
 export function HeroSection() {
