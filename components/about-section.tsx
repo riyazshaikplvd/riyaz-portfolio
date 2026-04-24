d397c959cc875b5d4d47a34d335bfee2edff52a3
RIYAZ SHAIK <riyazshaikplvd@gmail.com>
2026-04-24 17:16:23 +0530

    Update about-section.tsx

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -4,7 +4,7 @@ export function AboutSection() {
   return (
     <section
       id="about"
-      className="min-h-screen pt-10 px-4 sm:px-6 lg:px-8 scroll-mt-10 bg-slate-950"
+    className="min-h-[calc(100vh-80px)] pt-20 px-4 sm:px-6 lg:px-8 scroll-mt-20 bg-slate-950"
     >
       <div className="mx-auto max-w-5xl w-full text-center">
         
