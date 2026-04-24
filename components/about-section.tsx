92d16742877d476b2d6a11d6c708064f9cec57ac
RIYAZ SHAIK <riyazshaikplvd@gmail.com>
2026-04-24 12:59:38 +0530

    Update about-section.tsx

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -4,7 +4,7 @@ export function AboutSection() {
   return (
     <section
       id="about"
-      className="py-26 px-8 sm:px-10 lg:px-12 bg-slate-950"
+      className="py-28 px-10 sm:px-12 lg:px-12 bg-slate-950"
     >
       <div className="mx-auto max-w-5xl w-full text-center">
 
